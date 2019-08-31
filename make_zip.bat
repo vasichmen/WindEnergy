070d7504ba6b85a80dafbf08b240fff5fb3165de
vasichmen <vass.vass@mail.ru>
2019-08-31 10:24:07 +0300

    Release 1.4.0

---
+++ make_zip.bat
@@ -0,0 +1,9 @@
+chcp 1251
+
+Set /P ver=  введите версию для продолжения ^>
+echo %ver%
+Set n=windenergy_
+Set ext=.zip
+Set name=%n%%ver%%ext%
+echo %name%
+"C:\Program Files\7-Zip\7z.exe" a -tzip -ssw -mx7 -r0 -x@exclusions_arhive_program.txt "D:\Clouds\Projects\CS\WindEnergy\%name%" "D:\Clouds\Projects\CS\WindEnergy\UI\bin\Release\"
