be63ec34773f7503c9f21ee9f52b207fdb6b8f44
vasichmen <vass.vass@mail.ru>
2020-02-23 23:32:33 +0300

    Release 1.6.0

--- WindEnergy/Properties/AssemblyInfo.cs
+++ WindEnergy/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.1.0")]
-[assembly: AssemblyFileVersion("1.5.1.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
 [assembly: NeutralResourcesLanguage("ru")]
