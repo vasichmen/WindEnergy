87db37dbdbc368436ca24936e19c4664eca43833
Василий <vasily.mett@outlook.com>
2020-10-07 21:00:44 +0300

    Release 1.8.3

--- WindLib/Properties/AssemblyInfo.cs
+++ WindLib/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.2.0")]
-[assembly: AssemblyFileVersion("1.8.2.0")]
+[assembly: AssemblyVersion("1.8.3.0")]
+[assembly: AssemblyFileVersion("1.8.3.0")]
 [assembly: NeutralResourcesLanguage("ru")]
