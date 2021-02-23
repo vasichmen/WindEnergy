417554c14a7b7df74276a94f593be099184d073f
Василий <vasily.mett@outlook.com>
2021-02-23 17:36:06 +0300

    base class for interpolation

---
+++ WindLib/Transformation/Restore/Interpolation/BaseInterpolateMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindEnergy.WindLib.Transformation.Restore.Interpolation
+{
+   public abstract class BaseInterpolateMethod: IInterpolateMethod
+    {
+        protected StandartIntervals FromInterval;
+        protected StandartIntervals ToInterval;
+
+        protected  BaseInterpolateMethod(StandartIntervals from, StandartIntervals to)
+        {
+            FromInterval = from;
+            ToInterval = to;
+        }
+
+        public abstract double GetValue(double x);
+    }
+}
